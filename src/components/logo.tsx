d0dae2314d5358184ca135c50c54849d04ea902a
Italo Faria abrocese <ifariaabrocese@gmail.com>
2025-09-11 21:53:32 +0000

    troque por esse logo https://i.imgur.com/BbKtKQ4.png

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,7 +12,7 @@ export function Logo({ className }: { className?: string }) {
       )}
     >
       <Image 
-        src="/logo.png" 
+        src="https://i.imgur.com/BbKtKQ4.png" 
         alt="França Higienização Logo" 
         width={180} 
         height={50} 
