897220b0c8963005420865e6edfdd7e5b31e2876
Italo Faria abrocese <ifariaabrocese@gmail.com>
2025-09-15 23:12:09 +0000

    volta com e logo antiga

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,7 +12,7 @@ export function Logo({ className }: { className?: string }) {
       )}
     >
       <Image 
-        src="https://i.imgur.com/6cG4fLA.png" 
+        src="https://placehold.co/180x50/1E3A5F/FFFFFF?text=França+Higienização" 
         alt="França Higienização Logo" 
         width={180} 
         height={50} 
