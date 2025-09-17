b3280cbf226fd9b216cb092f56dc0dce22cc1685
Italo Faria abrocese <ifariaabrocese@gmail.com>
2025-09-17 00:32:13 +0000

    oq aconteceu o site ficou todo desmontado

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'i.imgur.com',
-        port: '',
-        pathname: '/**',
-      },
+      }
     ],
   },
 };
