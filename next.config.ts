2b2463f1f7988f5bed906b8a0ba8a9de381b890b
Italo Faria abrocese <ifariaabrocese@gmail.com>
2025-09-17 20:06:09 +0000

    consete isso meu Se este for o seu site e você não esperava um erro 404

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
